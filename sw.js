a8159c3846d4708e6f6fd7aeeab492bd50c606da
jkuria254 <jackwairegi25@gmail.com>
2026-09-05 04:22:29 +0300

    Update service worker for new cache version and files

--- sw.js
+++ sw.js
@@ -1,4 +1,15 @@
-const CACHE='tradelab-v3';
-const FILES=['./','./index.html','./manifest.json'];
-self.addEventListener('install', e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))); });
-self.addEventListener('fetch', e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
+const CACHE='tradelab-v5';
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll([
+    '/journalPWA/',
+    '/journalPWA/index.html',
+    '/journalPWA/manifest.json',
+    '/journalPWA/icon-192.png',
+    '/journalPWA/icon-512.png'
+  ])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); });
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
